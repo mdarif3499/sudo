1a5bcb3d736fdaa97609a09bbd0a9dc66a2c4d15
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-06-13 11:40:51 +0600

    Refactor ProfileMenuItem to use a Column layout and add optional divider

--- lib/profile/widget/profile_menu_item.dart
+++ lib/profile/widget/profile_menu_item.dart
@@ -6,6 +6,7 @@ import '../../component/text/common_text.dart';
 class ProfileMenuItem extends StatelessWidget {
   final String iconPath;
   final String title;
+  final bool dived;
   final String? trailingText;
   final Color? trailingTextColor;
   final Widget? trailing;
@@ -23,48 +24,71 @@ class ProfileMenuItem extends StatelessWidget {
     required this.onTap,
     required this.iconColor,
     required this.iconBgColor,
+    this.dived = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      contentPadding: EdgeInsets.zero,
-      leading: Image.asset(
-        iconPath,
-        color: iconColor,
-        height: 40.sp,
-        width: 40.sp,
-      ),
-      title: CommonText(
-        text: title,
-        fontSize: 16,
-        fontWeight: FontWeight.w400,
-        color: AppColors.textPrimaryColor,
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          if (trailingText != null)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 2.h),
-              margin: EdgeInsets.only(right: 8.w),
-              decoration: BoxDecoration(
-                color:
-                    trailingTextColor?.withValues(alpha: 0.1) ??
-                    Colors.grey.withValues(alpha: 0.1),
-                borderRadius: BorderRadius.circular(10.r),
-              ),
-              child: CommonText(
-                text: trailingText!,
-                fontSize: 10,
-                color: trailingTextColor ?? AppColors.textSecondaryColor,
-              ),
+    return Column(
+      children: [
+        InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 12.h),
+            child: Row(
+              children: [
+                // Icon Container
+                Container(
+                  padding: EdgeInsets.all(8.r),
+                  decoration: BoxDecoration(
+                    color: iconBgColor,
+                    borderRadius: BorderRadius.circular(8.r),
+                  ),
+                  child: Image.asset(
+                    iconPath,
+                    color: iconColor,
+                    height: 24.sp,
+                    width: 24.sp,
+                  ),
+                ),
+                SizedBox(width: 12.w),
+
+                // Title
+                Expanded(
+                  child: CommonText(
+                    text: title,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.textPrimaryColor,
+                  ),
+                ),
+
+                // Trailing Section
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (trailingText != null)
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 2.h),
+                        margin: EdgeInsets.only(right: 8.w),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        child: CommonText(
+                          text: trailingText!,
+                          fontSize: 10,
+                          color: trailingTextColor ?? AppColors.textSecondaryColor,
+                        ),
+                      ),
+                    trailing ?? Icon(Icons.arrow_forward_ios, size: 14.sp, color: Colors.grey),
+                  ],
+                ),
+              ],
             ),
-          trailing ??
-              Icon(Icons.arrow_forward_ios, size: 14.sp, color: Colors.grey),
-        ],
-      ),
+          ),
+        ),
+        if (dived) Divider(height: 1, thickness: 0.5),
+      ],
     );
   }
 }
