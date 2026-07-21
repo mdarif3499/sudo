48fd05ec83497a4121f08ea3b00cf23a11a66ddb
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-07-21 11:34:25 +0600

    Add period history feature with new model, controller methods, and UI updates for displaying cycle information

--- lib/auth/controller/otp_controller.dart
+++ lib/auth/controller/otp_controller.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../config/route/app_routes.dart';
-import '../../../services/api/api_client.dart';
 import '../../../services/api/api_service.dart';
 import '../../../config/api/api_end_point.dart';
 import '../../../utils/log/app_utils.dart';
